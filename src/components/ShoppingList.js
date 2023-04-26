611ec7d0a2d9167065af2a78a877910e0945a7b7
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-26 21:26:24 +0300

    added a useState to search item

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,31 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchTermChange(event) {
+    setSearchTerm(event.target.value);
+  }
 
-    return item.category === selectedCategory;
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return item.name.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+
+    return (
+      item.category === selectedCategory &&
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchTermChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
